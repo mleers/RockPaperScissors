fa778b596fd53595dfa9282f347e9aeb63909542
mleers <mleers4@gmail.com>
2022-10-09 14:17:16 -0700

    Add basic structure and text

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var gameChoice = 0
+    @State private var tryToWin = false
+    
+    @State private var choices = ["Rock", "Paper", "Scissors"]
+    @State private var gameChoiceEmoji = ["👊", "✋", "✌️"]
+    @State private var score = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 50) {
+            HStack {
+                Text("Try to")
+
+                if tryToWin {
+                    Text("win")
+                        .foregroundColor(.green)
+                } else {
+                    Text("lose")
+                        .foregroundColor(.red)
+                }
+                Text("vs:")
+            }
+            
+            Text(gameChoiceEmoji[gameChoice])
+                .font(.system(size: 100))
+            
+            HStack(spacing: 50) {
+                Button {
+                    // do something
+                } label: {
+                    Text("👊")
+                        .font(.system(size: 50))
+                }
+                Button {
+                    // do something
+                } label: {
+                    Text("✋")
+                        .font(.system(size: 50))
+                }
+                
+                Button {
+                    // do something
+                } label: {
+                    Text("✌️")
+                        .font(.system(size: 50))
+                }
+            }
+            
+            Text("Score: \(score)")
+        }
     }
 }
 
